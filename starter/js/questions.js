72539f89f687d7f29d9cbceb59aaf0f84fcf3a7f
Ebun Ahme <EBunAhme@Ebuns-MacBook-Pro.local>
2023-01-24 23:23:42 +0000

    Follow up commit

---
+++ starter/js/questions.js
@@ -0,0 +1,19 @@
+let questions = [
+    {
+        "title": "Who won the 2022 World cup in Qatar?",
+        "choices": ["Nigeria", "Portugal", "Argentina", "Qatar"],
+        "answer": "Argentina"
+        
+    },
+    {
+        "title": "Coding is fun and easy, if you dedicate time and effort.",
+        "choices": ["True", "False"],
+        "answer": "True"
+
+    },
+    {
+        "title": "Who will win the next General Election in Nigeria?",
+        "choices": ["Bola Ahmed Tinubu", "Peter Obi", "Atiku Abubakar", "No one knows!"  ],
+        "answer": "No one knows"
+    },
+]
